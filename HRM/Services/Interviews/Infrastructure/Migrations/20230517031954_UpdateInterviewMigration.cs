4aa99f2bda0cea61508d9d4cf03e54f717f23a0e
elainechiang <elainechiang99@gmail.com>
2023-05-16 23:28:22 -0400

    Finished Interview API with new files

---
+++ HRM/Services/Interviews/Infrastructure/Migrations/20230517031954_UpdateInterviewMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateInterviewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_JobStatusLookUps",
+                table: "JobStatusLookUps");
+
+            migrationBuilder.RenameTable(
+                name: "JobStatusLookUps",
+                newName: "InterviewStatusLookUps");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_InterviewStatusLookUps",
+                table: "InterviewStatusLookUps",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_InterviewStatusLookUps",
+                table: "InterviewStatusLookUps");
+
+            migrationBuilder.RenameTable(
+                name: "InterviewStatusLookUps",
+                newName: "JobStatusLookUps");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_JobStatusLookUps",
+                table: "JobStatusLookUps",
+                column: "Id");
+        }
+    }
+}
