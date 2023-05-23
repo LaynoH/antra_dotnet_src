e794fcdab94b89b73ccd44c71bf9e50140d07884
elainechiang <elainechiang99@gmail.com>
2023-05-22 20:03:53 -0400

    Authentication framework

--- HRM/Services/Authentication/Authentication.API/Program.cs
+++ HRM/Services/Authentication/Authentication.API/Program.cs
@@ -1,3 +1,8 @@
+using Authentication.API.Data;
+using Authentication.API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AuthenticationDbContext>
+(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuthenticationDbConnection")));
+
+// specific to Identity Database
+builder.Services.AddIdentity<User, Role>()
+    .AddEntityFrameworkStores<AuthenticationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
